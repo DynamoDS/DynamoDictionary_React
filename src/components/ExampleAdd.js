b04fbd1e37b25b1fce54b67d99f9f9add0d10148
Wilbert Mui <wmui51@yahoo.com>
2021-07-12 23:58:28 -0700

    add tooltip

--- src/components/ExampleAdd.js
+++ src/components/ExampleAdd.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import { withStyles } from "@material-ui/core/styles";
 
-
+const StyledTooltip = withStyles({
+  popper: {
+    top: "15px !important"
+  },
+  tooltipPlacementTop: {
+    borderRadius: "2px",
+    fontSize: "14px"
+  }
+})(Tooltip);
 
 export default function ExampleAdd(props){
-    return(
-      <IconButton width="40px" tooltip='Add Example File' touch={true} tooltipPosition="top-center" style={{"padding": "0", 'margin':'0', "color":"gray"}}  onClick={()=>{props.exAdd(props.node)}}>
-      <img alt='add example file' src="images/icons/add.svg"/>
-      </IconButton>
-    )
+    return (
+      <StyledTooltip title="Add Example File" placement="top" onClick={()=>{props.exAdd(props.node)}}>
+        <IconButton size="medium">
+          <img alt='add example file' src="images/icons/add.svg" style={{width: "40px"}} />
+        </IconButton>
+      </StyledTooltip>
+    );
 }
